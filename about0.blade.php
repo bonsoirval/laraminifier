f8c8c5a966f4f411db9659f18c653307b772139e
Njoku Okechukwu Valentine <bonsoirval@gmail.com>
2019-03-29 21:46:08 +0100

    Program working fine now

---
+++ about0.blade.php
@@ -0,0 +1,58 @@
+@extends('layouts.frontend.frontend')
+@section('content')
+<section class="flat-row no-padding">
+    <div class="flat-fluid">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="post">
+                        <h1 class="title">Our Brief History</h1>
+                        <p>The schools of Nursing and Midwifery gained their recognition in 1954 and 1965 respectively. In 1959, the Hospital was recognized for the per-registration training of doctors in medicine, surgery, obstetrics and gynecology.
+In Mid 1970s, it got the status of a Teaching Hospital approved for Post Graduate Training of Doctors in General Medicine and Housemanship in Surgery, Internal Medicine, and Gynecology.
+
+The Hospital, which stared with 200 beds later increased to 320 beds in 1967. It has, right from its foundation, served as a referral center next to the university Teaching Hospitals. The unfortunate Nigeria-Biafran Civil war (1967-170) retarded the growth of the Hospital. At the end of the war, the Hospital's infrastructure was destroyed. Hospital equipment were lost, and worse still, dedicated pioneer expatriate staff of the  hospital had to leave the country. Post war efforts as the revamping of the Hospital have not been succeful due to the prevailing country's economic situation.
+</p>
+
+                    </div>
+                </div>
+
+                <div class="col-md-6">
+                    <div class="post">
+                        <h1 class="title"><br/></h1>
+                        <p> However, the Hospital is doing her patience's / clients. Presently, the hospital enjoys moral, economic, financial and spiritual support as sources of sustainability from His Grace, Most Rev. Dr Anthony J.V. Obinna, who became the Archbishop and third Bishop of the owerri Archdiocese in in 1993.
+The hospital is divided into Wards, Departments and Units as follows:
+
+•  The Medical Service Departments
+
+•  The Nursing Service Department
+
+•  The Laboratory Department
+
+•  The Pharmacy Department
+
+•  The Health Care Unit
+
+•  The Training Schools
+
+•  The Mortuary and Embalmment Unit
+
+•  The Kitchen Unit & •  The Chest Unit.
+
+Now the hospital can boast of additional new Ultra Modern Out Patient structure with wards, department and units. All these will help the hospital to realize her vision.</p>
+
+                    </div>
+                </div>
+
+<!--                        <div class="col-md-4">
+                    <div class="post">
+                        <h1 class="title">GLOBAL RECOGNITION</h1>
+                        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+                        <a class="flat-button button-button_30 btn-default " href="#" data-delay="0">READ MORE <i class="fa fa-angle-right"></i></a>
+                    </div>
+                </div>-->
+            </div><!--/row-->
+        </div><!--/flat-fluid-->
+    </div><!--/container-->
+</section><!--/flat-row -->
+
+@endsection
--- laraminifier.py
+++ laraminifier.py
@@ -5,18 +5,17 @@ import re
 
 file_path = 'about.blade.php' #file to be optimised
 
-def replace(file_path, pattern, subst):
+def replace(file_path):#, pattern, subst):
     #Create temp file
     fh, abs_path = mkstemp()
     with fdopen(fh,'w') as new_file:
         with open(file_path) as old_file:
             for line in old_file:
-                line = re.sub(r"<!--(.|\s|\n|[ \t]+)*?-->", "", line)
-                #line = re.sub(r">(\s+)<", "><", line)
-                new_file.write(line.replace(pattern, subst))
+                line = ' '.join(line.split()) #remove all whitespaces from the html file line
+                line = re.sub(r"<!--(.|\s|\n)*?-->", "", line) #remove all html comment
+                new_file.write(line) #write / save the lines to the file
     #Remove original file
     remove(file_path)
     #Move new file
     move(abs_path, file_path)
-replace(file_path, '\n','')
-#replace(file_path, '', '')
+replace(file_path)
